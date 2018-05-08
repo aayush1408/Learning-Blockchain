bea814c76e712355cda01e332cf1cd8788b2c930
aayush1408 <aayushjaiswal0114@gmail.com>
2018-05-08 22:43:31 +0530

    chain created

---
+++ main.js
@@ -0,0 +1,35 @@
+const SHA256 = require('crypto-js/sha256');
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash;
+    }
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+    createGenesisBlock() {
+        return new Block(0, "01/05/2018", "Genesis Block", "0");
+    }
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let savjeCoin = new Blockchain();
+savjeCoin.addBlock(new Block(1, "03/05/2018", { amount: 5 }));
+savjeCoin.addBlock(new Block(2, "04/05/2018", { amount: 10 }));
+console.log(JSON.stringify(savjeCoin, null, 4));
